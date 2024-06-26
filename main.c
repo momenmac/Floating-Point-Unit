d9532a41e31ee66256db2e4854ce438939e29ae1
momen_anani <momanani2017@gmail.com>
2024-06-26 22:04:23 +0300

    Initial commit

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+struct MyFloat {
+    int fraction;
+    int exponent;
+    int sign;
+};
+
+struct MyFloat Myfloat(float x);
+
+int main(void) {
+
+}
+struct MyFloat Myfloat(float x){
+    struct MyFloat xs;
+    if(x==0){
+        xs.exponent =0;
+        xs.fraction =0;
+        xs.exponent =0;
+    } else{
+        
+    }
+}
