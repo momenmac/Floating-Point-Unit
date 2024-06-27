1fc8315a0e07cdceecd1763642b5ba6d36c764f8
mo-matar <116981460+mo-matar@users.noreply.github.com>
2024-06-27 18:50:40 +0300

    more test cases

--- tb_FPU.v
+++ tb_FPU.v
@@ -44,21 +44,40 @@ module tb_FPU;
     
     initial begin	
 		clk <= 0;
-		#20;
+		F1 <= 0;
+		F2 <= 0; 
+		#700;
 		F1 <= 32'h3f400000;
-		F2 <= 32'h3ee00000;	 
-		#300;
+		F2 <= 32'h3ee00000;
+		//F3 should be: 0x3f980000;
+		#700;
 		F1 <= 32'h3D99999A;
 		F2 <= 32'h3BF5C28F;
-		//F3 should be: 0x3f980000;
+		//F3 should be: 0x3DA8F5C3;
+		#700;
+		F1 <= 32'h3F000000;
+		F2 <= 32'hBEE00000;
+		//F3 should be: 0x3D800000;
+		#700;
+		F1 <= 32'hBF000000;
+		F2 <= 32'h3EE00000;
+		//F3 should be: 0xBD800000;
+		#700;
+		F1 <= 32'hBF000000;
+		F2 <= 32'h3BEE0000;
+		//F3 should be: 0xBF700000;	
+		#700;
+		F1 <= 32'h3F7FFFFF;
+		F2 <= 32'h3F000000;
+		#700;
+		F1 <= 32'h3FFFFFFF;
+		F2 <= 32'h40000000;
+		#700;
+		F1 <= 32'h7F7FFFFF;
+		F2 <= 32'h7F000000;
 		#6000 $finish;
     end
 
-  initial begin
-    forever begin
-      #300 clk <= ~clk; 
-    end
-  end
     endmodule
 
 
